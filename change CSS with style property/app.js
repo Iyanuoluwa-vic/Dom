d84d662ba9218904b754ea6b7bc73628af1d6f2f
Adeyeri Iyanuoluwa <Adeyeriiyanu@gmail.com>
2022-09-03 17:43:15 -0500

    none

--- change CSS with style property/app.js
+++
@@ -1,8 +0,0 @@
-// this is used to add style to css
-const random = document.querySelector(".random");
-
-random.style.backgroundColor = "red";
-random.style.color = "blue";
-
-// this is used to add the css property by adding the class of name title
-random.classList.add("title");
