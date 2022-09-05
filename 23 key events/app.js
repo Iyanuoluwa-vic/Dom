678ee7b694f6d6e3f0911bfde9a85a02c5c85767
Adeyeri Iyanuoluwa <Adeyeriiyanu@gmail.com>
2022-09-05 18:35:58 -0500

    key events

---
+++ 23 key events/app.js
@@ -0,0 +1,13 @@
+const nameInput = document.getElementById("name");
+
+// nameInput.addEventListener("keypress", function () {
+//   console.log("this is an imput");
+// });
+
+// nameInput.addEventListener("keyup", function () {
+//   console.log("this is an imput");
+// });
+
+nameInput.addEventListener("keyup", function () {
+  console.log(nameInput.value);
+});
