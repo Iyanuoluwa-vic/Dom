863382accfa02894bbde94ea2b538dbbded94fa5
Adeyeri Iyanuoluwa <Adeyeriiyanu@gmail.com>
2022-09-05 18:58:03 -0500

    definitioin of keyboard events

--- 23 key events/app.js
+++ 23 key events/app.js
@@ -1,3 +1,5 @@
+// KeyboardEvent events just indicate what interaction the user had with a key on the keyboard at a low level, providing no contextual meaning to that interaction\
+
 const nameInput = document.getElementById("name");
 
 // nameInput.addEventListener("keypress", function () {
