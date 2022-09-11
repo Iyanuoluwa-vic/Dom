dd10811a5302aab67152c8e5158ef3fe04414f48
Adeyeri Iyanuoluwa <Adeyeriiyanu@gmail.com>
2022-09-10 20:58:05 -0500

    event propagation example

---
+++ 26 Event Propagation example/app.js
@@ -0,0 +1,20 @@
+const container = document.querySelector(".container");
+const btn = document.querySelector(".btn");
+const heading = document.querySelector(".heading");
+
+btn.addEventListener("click", function () {
+  const element = document.createElement("h1");
+  element.classList.add("heading");
+  element.textContent = "i'm in the heading";
+  container.appendChild(element);
+});
+
+container.addEventListener("click", function (e) {
+  if (e.target.classList.contains("heading")) {
+    console.log("it contain the heading");
+  }
+});
+
+// heading.addEventListener("click", function () {
+//   console.log("heading");
+// });
